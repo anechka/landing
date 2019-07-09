1052d6ca883223078aa2430732c3b982dd54b858
Anna Novikova <anna@novikova.us>
2019-07-10 00:38:20 +0300

    Added tooltips and card styles.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 console.log("hello");
+
+$(function() {
+    $('li.dropdown').hover(function(){ //Open on hover
+            $('.dropdown-menu.header-dropdown').addClass("show");
+            console.log("jquery")
+        },
+        function(){ //Close when not hovered
+            $('.header-dropdown').removeClass("show");
+        });
+});
