7075dda23b492fd90bd64b1ef26d1bb3c1f157b7
Andrey Menangen <menangen@gmail.com>
2020-03-24 19:30:57 +0300

    Fixed bug arrow-icon transform translate 3d

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 console.log("hello");
 
 $(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+
     for(let i = 1; i < 5; i++) {
         $('li#navbarDropdown' + i ).hover(function(){ //Open on hover
                 $('#menu-' + i).addClass("show");
