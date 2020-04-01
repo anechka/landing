2c424c48992361082a753e8cf282d81559ebf267
Andrey Menangen <menangen@gmail.com>
2020-04-01 18:19:34 +0300

    Mobile styles, tooptip

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,6 @@ $(function() {
     for(let i = 1; i < 5; i++) {
         $('li#navbarDropdown' + i ).hover(function(){ //Open on hover
                 $('#menu-' + i).addClass("show");
-                console.log("jquery")
             },
             function(){ //Close when not hovered
                 $('#menu-' + i).removeClass("show");
